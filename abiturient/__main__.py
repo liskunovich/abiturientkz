e6d22eea18d7fab1cd04837c41d787fa0437d2d4
David <devid02ru@gmail.com>
2024-05-19 20:56:50 +0500

    Registred new microservice: profile

--- abiturient/__main__.py
+++ abiturient/__main__.py
@@ -3,14 +3,16 @@ from abiturient.utils.config_reader import get_config
 if __name__ == "__main__":
     import uvicorn
     from abiturient.utils.app_start import get_current_app, connect_app
-    from abiturient.apps.about.router import about_router
+    from apps.about.router import about_router
     from apps.auth.router import auth_router
+    from apps.profile.router import profile_router
 
     from fastapi import FastAPI
 
     app: FastAPI = get_current_app()
     connect_app(app, '/about', about_router)
     connect_app(app, '/auth', auth_router)
+    connect_app(app, '/profile', profile_router)
     config = uvicorn.Config(
         app,
         host='0.0.0.0',
